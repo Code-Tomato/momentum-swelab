c4ef646ddb866f5a47bf4e201c6c0e65620f5853
Judah Thurman <jnt979@utexas.edu>
2025-11-12 20:38:10 -0600

    fix another import issue in portal

--- client/src/pages/MyUserPortal.js
+++ client/src/pages/MyUserPortal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, Navigate } from 'react-router-dom';
 
 const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
