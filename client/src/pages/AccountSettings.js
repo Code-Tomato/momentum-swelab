391a63a808f7d25417f3fe4b3ed636150c8be895
Nathan <nathayoha@gmail.com>
2025-11-14 03:15:56 -0600

    fix

--- client/src/pages/AccountSettings.js
+++ client/src/pages/AccountSettings.js
@@ -11,7 +11,6 @@ function AccountSettings() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
